60712659efb38d92b30793b045e16f1555f9753b
Joao <joaosantana@ntopus.com.br>
2025-04-03 14:31:20 -0300

    test

---
+++ testdata/src/testlintdata/funcbody/test.go
@@ -0,0 +1,33 @@
+package funcbody
+
+const a = 2
+const b = 3
+
+const g = 0
+
+const f = 0
+
+func test1() int {
+	c := 1
+
+	d := 1
+
+	return c + d
+}
+func test2() {
+
+}
+
+func test3() int {
+	c := 1
+	// explain explain
+	d := 1
+	// explain explain
+	return c + d
+}
+
+type test4 struct{}
+type test5 interface{}
+
+//comment
+type test6 struct{}
